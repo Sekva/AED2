beb09e499463613fd0e642a156fcf2319d387f35
Matheus Machado <matheusmachado708@gmail.com>
2018-05-10 22:41:12 -0300

    teste checagem de altura

--- ABB/src/main.cpp
+++ ABB/src/main.cpp
@@ -11,6 +11,8 @@ int main() {
 	No n3(10);
 	No n4(34);
 	No n5(8);
+	No n6(333333);
+	No n7(99999999);
 
 	Arvore t;
 
@@ -19,7 +21,8 @@ int main() {
 	t.inserirNo(&n3);
 	t.inserirNo(&n4);
 	t.inserirNo(&n5);
-	
+	t.inserirNo(&n6);
+	t.inserirNo(&n7);	
 	std::cout << n.checarAltura() << std::endl;
 	return 0;
 
